8cf87a762aea236b323f906e339c4761bedf7116
Xinlong Yi <xinlong1996@gmail.com>
2020-10-28 01:04:25 -0700

    feat: Finished most part of Vincent
    Finished the most part of Vincent Method. But meet problem with how to calculate with power operation. Need to be done tomorrow morning
    
    Message footer

--- src/vincent.cpp
+++ src/vincent.cpp
@@ -1,9 +1,49 @@
 #include "vincent.h"
 
 vector<tuple<double, double>> Vincent::isoRoot(Poly& p) {
-    return {}
-}
+  vector<tuple<double, double>> ret;
+  double maxOfRoot = bound(p);
 
+  // Init L
+  deque<tuple<Poly, Poly>> L;
+  vector<double> tmpCoef = {1, 0};
+  Poly tmpPoly = Poly(tmpCoef);
+  L.emplace_back(tuple<Poly, Poly>(p, tmpPoly));
+  tmpCoef[0] = -1;
+  tmpPoly = Poly(tmpCoef);
+  L.emplace_back(tuple<Poly, Poly>(timeToP(p, -1), tmpPoly));
+
+  while (L.size() > 0) {
+    auto frontTuple = L.front();
+    L.pop_front();
+    Poly A = get<0>(frontTuple), M = get<1>(frontTuple);
+    int v = signChangeNum(A, 0);
+    if (v == 0) continue;
+    if (v == 1)
+      ret.emplace_back(
+          tuple<double, double>(M.valueAt(0),fmax(-maxOfRoot, fmin(maxOfRoot, M.valueAt(INFINITY))))
+          );
+    
+    // TODO: Optimize b
+    int b = 1;
+    Poly B = addToP(A, b);
+    int w = v - signChangeNum(B, 0);
+    if(B.valueAt(0) == 0){
+      ret.emplace_back(tuple<double, double>(M.valueAt(b), M.valueAt(b)));
+      tmpCoef = {1, 0};
+      tmpPoly = Poly(tmpCoef);
+      B /= tmpPoly;
+    }
+
+    L.emplace_back(tuple<Poly, Poly>(B, addToP(M, b)));
+    if(w == 0) continue;
+    if(w==1) ret.emplace_back(M.valueAt(0), M.valueAt(b));
+    if(w > 1) continue; // TODO: util and reload ^ operator
+
+  }
+
+  return {};
+}
 
 vector<double> Vincent::solve(Poly& p) {
   if (p.deg() == 1) return {-p[1] / p[0]};
